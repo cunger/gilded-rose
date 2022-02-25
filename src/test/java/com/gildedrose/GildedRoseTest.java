df2ffb25ba1427fef5881dea3d14858bab8b559c
christina <andrea.christina.unger@gmail.com>
2022-02-25 17:21:14 +0100

    refactor: nest tests and generate more readable display name

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,28 +1,32 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static com.gildedrose.GildedRose.*;
 import static org.assertj.core.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class GildedRoseTest {
 
     @Test
-    void itemsWithNegativeQualityAreSetToMinQuality() {
+    void items_with_negative_quality_are_set_to_minimal_quality() {
         GildedRoseExample example = new GildedRoseExample("Junk", 2, MIN_QUALITY - 1);
 
         assertThat(example.quality()).isEqualTo(MIN_QUALITY);
     }
 
     @Test
-    void itemsWithTooHighQualityAreSetToMaxQuality() {
+    void items_with_too_high_quality_are_set_to_max_quality() {
         GildedRoseExample example = new GildedRoseExample("All-in-one problem solver", 2, MAX_QUALITY + 1);
 
         assertThat(example.quality()).isEqualTo(MAX_QUALITY);
     }
 
     @Test
-    void sellDateGetsCloserWithEachUpdate() {
+    void sell_date_gets_closer_with_each_update() {
         GildedRoseExample example = new GildedRoseExample("Cookbook", 2, 5);
         int initialSellIn = example.sellIn();
 
@@ -31,28 +35,31 @@ class GildedRoseTest {
         assertThat(example.sellIn()).isLessThan(initialSellIn);
     }
 
-    @Test
-    void normalQualityDecreases() {
-        GildedRoseExample example = new GildedRoseExample("+5 Dexterity Vest", 10, 20);
-        int initialQuality = example.quality();
+    @Nested
+    class normal_quality {
+        @Test
+        void decreases() {
+            GildedRoseExample example = new GildedRoseExample("+5 Dexterity Vest", 10, 20);
+            int initialQuality = example.quality();
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isLessThan(initialQuality);
+            assertThat(example.quality()).isLessThan(initialQuality);
+        }
+
+        @Test
+        void decreases_twice_as_fast_once_expired() {
+            GildedRoseExample example = new GildedRoseExample("Elixir of the Mongoose", 0, 20);
+            int initialQuality = example.quality();
+
+            example.update();
+
+            assertThat(example.quality()).isEqualTo(initialQuality - 2 * QUALITY_CHANGE);
+        }
     }
 
     @Test
-    void normalQualityDecreasesTwiceAsFastOnceSellDateHasPassed() {
-        GildedRoseExample example = new GildedRoseExample("Elixir of the Mongoose", 0, 20);
-        int initialQuality = example.quality();
-
-        example.update();
-
-        assertThat(example.quality()).isEqualTo(initialQuality - 2 * QUALITY_CHANGE);
-    }
-
-    @Test
-    void qualityDoesNotDecreaseBelowMinQuality() {
+    void quality_does_not_decrease_below_minimum() {
         GildedRoseExample example = new GildedRoseExample("Crap", 10, MIN_QUALITY);
 
         example.update();
@@ -61,29 +68,41 @@ class GildedRoseTest {
     }
 
     @Test
-    void sulfurasQualityDoesNotDecrease() {
-        GildedRoseExample example = new GildedRoseExample("Sulfuras, Hand of Ragnaros", -1, 10);
-        int initialQuality = example.quality();
+    void quality_does_not_increase_above_maximum() {
+        GildedRoseExample example = new GildedRoseExample("Aged Brie", 10, MAX_QUALITY);
 
         example.update();
 
-        assertThat(example.quality()).isEqualTo(initialQuality);
+        assertThat(example.quality()).isEqualTo(MAX_QUALITY);
+    }
+
+    @Nested
+    class sulfuras {
+        @Test
+        void does_not_need_to_be_sold() {
+            GildedRoseExample example = new GildedRoseExample("Sulfuras, Hand of Ragnaros", 2, 5);
+            int initialSellIn = example.sellIn();
+
+            assertThat(initialSellIn).isLessThan(0);
+
+            example.update();
+
+            assertThat(example.sellIn()).isEqualTo(initialSellIn);
+        }
+
+        @Test
+        void quality_does_not_decrease() {
+            GildedRoseExample example = new GildedRoseExample("Sulfuras, Hand of Ragnaros", -1, 10);
+            int initialQuality = example.quality();
+
+            example.update();
+
+            assertThat(example.quality()).isEqualTo(initialQuality);
+        }
     }
 
     @Test
-    void sulfurasDoesNotNeedToBeSold() {
-        GildedRoseExample example = new GildedRoseExample("Sulfuras, Hand of Ragnaros", 2, 5);
-        int initialSellIn = example.sellIn();
-
-        assertThat(initialSellIn).isLessThan(0);
-
-        example.update();
-
-        assertThat(example.sellIn()).isEqualTo(initialSellIn);
-    }
-
-    @Test
-    void conjuredQualityDecreasesTwiceAsFastAsNormalItems() {
+    void conjured_quality_decreases_twice_as_fast_as_normal_items() {
         GildedRoseExample example = new GildedRoseExample("Conjured Mana Cake", 1, 6);
         int initialQuality = example.quality();
 
@@ -103,7 +122,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void brieQualityIncreasesWithAge() {
+    void brie_quality_increases_with_age() {
         GildedRoseExample example = new GildedRoseExample("Aged Brie", 10, 20);
         int initialQuality = example.quality();
 
@@ -112,51 +131,71 @@ class GildedRoseTest {
         assertThat(example.quality()).isGreaterThan(initialQuality);
     }
 
-    @Test
-    void qualityNeverIncreasesAboveMaxQuality() {
-        GildedRoseExample example = new GildedRoseExample("Aged Brie", 10, MAX_QUALITY);
+    @Nested
+    class backstage_pass_quality {
+        @Test
+        void increases_with_age() {
+            GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+            int initialQuality = example.quality();
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isEqualTo(MAX_QUALITY);
-    }
+            assertThat(example.quality()).isGreaterThan(initialQuality);
+        }
 
-    @Test
-    void backstagePassQualityIncreasesWithAge() {
-        GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-        int initialQuality = example.quality();
+        @Test
+        void increases_by_2_when_10_days_or_less_left() {
+            GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 11, 5);
+            int initialQuality = example.quality();
+            int nextQuality;
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isGreaterThan(initialQuality);
-    }
+            assertThat(example.quality()).isEqualTo(initialQuality + 1 * QUALITY_CHANGE);
 
-    @Test
-    void backstagePassQualityIncreasesBy2When10DaysOrLessLeft() {
-        GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 10, 5);
-        int initialQuality = example.quality();
+            nextQuality = example.quality();
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isEqualTo(initialQuality + 2 * QUALITY_CHANGE);
-    }
+            assertThat(example.quality()).isEqualTo(nextQuality + 2 * QUALITY_CHANGE);
 
-    @Test
-    void backstagePassQualityIncreasesBy3When5DaysOrLessLeft() {
-        GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 5, 5);
-        int initialQuality = example.quality();
+            nextQuality = example.quality();
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isEqualTo(initialQuality + 3 * QUALITY_CHANGE);
-    }
+            assertThat(example.quality()).isEqualTo(nextQuality + 2 * QUALITY_CHANGE);
+        }
 
-    @Test
-    void backstagePassQualityDropsToZeroOnceSellDateHasPassed() {
-        GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+        @Test
+        void increases_by_3_when_5_days_or_less_left() {
+            GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 6, 5);
+            int initialQuality = example.quality();
+            int nextQuality;
 
-        example.update();
+            example.update();
 
-        assertThat(example.quality()).isEqualTo(0);
+            assertThat(example.quality()).isEqualTo(initialQuality + 2 * QUALITY_CHANGE);
+
+            nextQuality = example.quality();
+
+            example.update();
+
+            assertThat(example.quality()).isEqualTo(nextQuality + 3 * QUALITY_CHANGE);
+
+            nextQuality = example.quality();
+
+            example.update();
+
+            assertThat(example.quality()).isEqualTo(nextQuality + 3 * QUALITY_CHANGE);
+        }
+
+        @Test
+        void drops_to_zero_once_expired() {
+            GildedRoseExample example = new GildedRoseExample("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+
+            example.update();
+
+            assertThat(example.quality()).isEqualTo(0);
+        }
     }
 }
